987a8dbcf3546370d8c23059312fd7d3c0ab855e
kaban54 <tikhonov.as@phystech.edu>
2023-04-03 03:22:03 +0300

    4 -> 8

--- 2.cpp
+++ 2.cpp
@@ -7,8 +7,8 @@
 
 void DrawMandelbrot ();
 
-const int W = 600;
-const int H = 600;
+const int W = 800;
+const int H = 800;
 const float MAXR2 = 100;
 const int MAXITER = 256;
 
@@ -29,9 +29,9 @@ void DrawMandelbrot ()
     float dx = (maxX - minX) / W;
     float dy = (maxY - minY) / H;
 
-    float x2 [4] = {};
-    float y2 [4] = {};
-    float xy [4] = {};
+    float x2 [8] = {};
+    float y2 [8] = {};
+    float xy [8] = {};
 
     float x    = 0, y    = 0;
     int   xpix = 0, ypix = 0;
@@ -47,23 +47,23 @@ void DrawMandelbrot ()
     {
         for (ypix = 0, y = minY; ypix < H ; ypix++, y += dy)
         {
-            for (xpix = 0, x = minX; xpix < W; xpix += 4, x += dx * 4)
+            for (xpix = 0, x = minX; xpix < W; xpix += 8, x += dx * 8)
             {
-                float x0    [4] = {x, x + dx, x + dx * 2, x + dx * 3};
-                float xnext [4] = {x, x + dx, x + dx * 2, x + dx * 3};
-                float y0    [4] = {y, y, y, y};
-                float ynext [4] = {y, y, y, y};
+                float x0    [8] = {x, x + dx, x + dx * 2, x + dx * 3, x + dx * 4, x + dx * 5, x + dx * 6, x + dx * 7};
+                float xnext [8] = {x, x + dx, x + dx * 2, x + dx * 3, x + dx * 4, x + dx * 5, x + dx * 6, x + dx * 7};
+                float y0    [8] = {y, y, y, y, y, y, y, y};
+                float ynext [8] = {y, y, y, y, y, y, y, y};
 
-                int count [4] = {-1, -1, -1, -1};
+                int count [8] = {-1, -1, -1, -1, -1, -1, -1, -1};
 
                 int out = 0;
 
                 for (int iter = 0; iter < MAXITER; iter++)
                 {
-                    for (i = 0; i < 4; i++) x2 [i] = xnext [i] * xnext [i];
-                    for (i = 0; i < 4; i++) y2 [i] = ynext [i] * ynext [i];
-                    for (i = 0; i < 4; i++) xy [i] = xnext [i] * ynext [i];
-                    for (i = 0; i < 4; i++)
+                    for (i = 0; i < 8; i++) x2 [i] = xnext [i] * xnext [i];
+                    for (i = 0; i < 8; i++) y2 [i] = ynext [i] * ynext [i];
+                    for (i = 0; i < 8; i++) xy [i] = xnext [i] * ynext [i];
+                    for (i = 0; i < 8; i++)
                     {
                         if (!(out & (1 << i)) && x2 [i] + y2 [i] > MAXR2)
                         {
@@ -77,13 +77,13 @@ void DrawMandelbrot ()
                         }
                     }
 
-                    if (out == 0b1111) break;
+                    if (out == 0b11111111) break;
 
-                    for (i = 0; i < 4; i++) xnext [i] = x2 [i] - y2 [i] + x0 [i];
-                    for (i = 0; i < 4; i++) ynext [i] = 2 * xy [i]      + y0 [i];
+                    for (i = 0; i < 8; i++) xnext [i] = x2 [i] - y2 [i] + x0 [i];
+                    for (i = 0; i < 8; i++) ynext [i] = 2 * xy [i]      + y0 [i];
                 }
 
-                for (i = 0; i < 4; i++)
+                for (i = 0; i < 8; i++)
                 {
                     if (count [i] == -1) txSetPixel (xpix + i, ypix, RGB (0, 0, 0));
                     else                 txSetPixel (xpix + i, ypix, RGB (count [i] % 256, 0, count [i] % 256));
